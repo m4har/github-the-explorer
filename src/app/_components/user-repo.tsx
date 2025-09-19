b1702319a7610b72a7dee3bd103bffb8347cc15e
Mahardicka <mahardicka404@gmail.com>
2025-09-19 08:43:37 +0700

    missing key map

--- src/app/_components/user-repo.tsx
+++ src/app/_components/user-repo.tsx
@@ -16,8 +16,14 @@ export const UserRepo = ({ user }: Props) => {
   const { data, isFetching } = useUserRepositories({ username: user.login });
   const repoList = data || [];
   return (
-    <AccordionItem value={user.id.toString()} data-testid={`user-repo-item-${user.id}`}>
-      <AccordionTrigger className="bg-gray-200 px-2 my-2" data-testid={`user-repo-trigger-${user.id}`}>
+    <AccordionItem
+      value={user.id.toString()}
+      data-testid={`user-repo-item-${user.id}`}
+    >
+      <AccordionTrigger
+        className="bg-gray-200 px-2 my-2"
+        data-testid={`user-repo-trigger-${user.id}`}
+      >
         {user.login}
       </AccordionTrigger>
       <AccordionContent className="flex flex-col gap-4 text-balance px-2 ml-4">
@@ -25,7 +31,11 @@ export const UserRepo = ({ user }: Props) => {
           <p data-testid={`user-repo-loading-${user.id}`}>Loading...</p>
         ) : (
           repoList.map((repo) => (
-            <div className="flex flex-col gap-2 bg-gray-300 p-2" data-testid={`user-repo-${repo.id}`}>
+            <div
+              key={repo.id}
+              className="flex flex-col gap-2 bg-gray-300 p-2"
+              data-testid={`user-repo-${repo.id}`}
+            >
               <div className="flex justify-between">
                 <span className="font-bold">{repo.name}</span>
                 <span className="flex items-center gap-1">
